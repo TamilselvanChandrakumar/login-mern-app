8dc67fa68cbdafe641979f5fdf4fd70857629274
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-24 20:32:13 +0530

    first commit

--- server/router/router.js
+++ server/router/router.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+
 const auth = require("../middleware/auth.js");
+
 const controller = require("../controller/controller.js");
+
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.delete("/delete", auth, controller.delete);
+
 module.exports = router;
